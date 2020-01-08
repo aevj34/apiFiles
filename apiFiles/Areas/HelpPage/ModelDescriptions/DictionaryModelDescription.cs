ec0d05f4e7277e3235ccca6f0726c1312fe090f9
aevj34 <anthony.varr@gmail.com>
2020-01-08 14:39:12 -0500

    Agregar archivos de proyecto.

---
+++ apiFiles/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace apiFiles.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ apiFiles/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="apiFiles.WebApiApplication" Language="C#" %>
